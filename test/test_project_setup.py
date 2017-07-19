6e144b893dba0dc2dbe50ab702ac6371c7db97d1
Malveen <edwari@C02SQ3LNG8WL.local>
2017-07-19 13:49:45 +0100

    Testable Functions

---
+++ test/test_project_setup.py
@@ -0,0 +1,20 @@
+from tracking_code_parsing.project_setup import read_file_data
+
+import unittest
+
+
+class TestMyFunctions(unittest.TestCase):
+    def test_read_file_data(self):
+        import json
+        sample_json = {
+                      'name' : 'John',
+                      'shares' : 100,
+                      'price' : 1230.23
+                      }
+        sample_json = json.dump(sample_json, ensure_ascii=False)
+        path = / path / to / file
+        filename = testcase.json
+        self.assertEqual(read_file_data(filename, path), sample_json)
+
+if __name__ == '__main__':
+    unittest.main()
