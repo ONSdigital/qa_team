fa4907edd26ace5b54f142d4cf99efd0723b1027
Ian Edward <ian.edward@ons.gov.uk>
2017-07-31 14:31:29 +0100

    Documented and configurable paths update.

--- tracking_code_parsing/project_setup.py
+++ tracking_code_parsing/project_setup.py
@@ -1,5 +1,7 @@
-""" Author - Ian Edward"""
-
+""" Author - Ian Edward
+ -- Extract tracking code structured as matrix --
+ Input and Output files passed in as arguments
+"""
 from functools import reduce
 import operator
 import os
@@ -15,38 +17,40 @@ def parse_json_survey(filename, path):
     json_data = read_file_data(filename, path)
     paths = process_paths(json_data)
     e_track = extract_tracking(paths)
-    # getFromDict(json_data, e_track)
     all_info = getFromDict(json_data, e_track)
     write_to_csv(all_info)
 
+""" Read json file and return json_data object """
 def read_file_data(filename, path):
     os.chdir(path)
     with open(filename,  encoding="utf8") as data_file:
         json_data = json.load(data_file)
     return json_data
 
+""" Append the paths and nods to list"""
 def process_paths(json_data):
-    full_list = [] # List containing the paths
+    full_list = []
     for pat, nods in traverse(json_data):
         full_list.append(pat)
     return full_list
 
+""" Traverse through json object to separate keys from values """
 def traverse(d, path = []):
     if isinstance(d, dict):
         iterator = d.items()
     else:
         iterator = enumerate(d) # Add count to the path
 
-    # For loop to seperate the keys and values
+    # Seperate the keys and values
     for k, v in iterator:
         yield path + [k], v
         if isinstance(v, (dict, list)):
             for k, v in traverse(v, path + [k]):
                 yield k, v
 
+""" Select every path with tracking code """
 def extract_tracking(full_list):
     stored_paths = []
-    #For loop to select every path with tracking_code as key
     for line in full_list:
          if ('tracking_code' in line) and ('cells' not in line):
              logging.debug(line)
@@ -61,6 +65,7 @@ def extract_tracking(full_list):
 
     return stored_paths
 
+""" Using the paths append value to keys """
 def getFromDict(dataDict, *mapList):
     tempInfo = []
     for arg in mapList:
@@ -73,16 +78,14 @@ def getFromDict(dataDict, *mapList):
                         skipLevels = ("segment", "question", "notes")
                         if x not in skipLevels:
                             tempInfo.append((x, res[x]))
-        # print(tempInfo)
 
         return tempInfo
-
+"""Write the parsed data to csv file"""
 def write_to_csv(all_info):
 
-    # Function to write the parsed data to csv file
+
     output_csv = sys.argv[4]
     output_folder = sys.argv[3]
-    print("hey")
     with open(output_folder + output_csv, 'a') as out:
         csv_out = csv.writer(out)
         for row in all_info:
--- tracking_code_parsing/project_setup_notes.py
+++ tracking_code_parsing/project_setup_notes.py
@@ -1,75 +1,68 @@
-""" Author - Ian Edward
- - Extract all tracking codes structured as matrix and all the relevant sections up to the root -
- - Input and Output file passed in as arguments -
+""" Author Ian Edward
+-- Extract notes and corresponding tracking code --
+- Input and Output files passed in as arguments -
 """
 
 from functools import reduce
 import operator
-import os
 import copy
+import sys
 import csv
 import json
-import logging
-import sys
 
 
-logging.basicConfig(filename='test_matrix.log', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s' )
 
-jsonfiledir = sys.argv[1] # directory argument
-jsonfile = sys.argv[2] # json file argument
-os.chdir(jsonfiledir) # Json file directory Path
-json_file = jsonfile # Selection of Json file passed in as an argument
+jsonfilepath = sys.argv[1] # Path to json file
 
-
-with open(json_file,  encoding="utf8") as data_file:
+with open(jsonfilepath, encoding="utf8") as data_file:
     data_202 = json.load(data_file)
 
-"""Traversing through json file collecting the json paths"""
+
 def traverse(d, path = []):
     if isinstance(d, dict):
         iterator = d.items()
     else:
         iterator = enumerate(d) # Add count to the path
 
-    # Seperate the keys and values
     for k, v in iterator:
         yield path + [k], v
         if isinstance(v, (dict, list)):
+
             for k, v in traverse(v, path + [k]):
+
                 yield k, v
-full_list = [] # List containing the paths
+
+full_list = []
+
 for pat, nods in traverse(data_202):
+
     full_list.append(pat)
 
+print("_____________")
+
+
+
+question_list = []
 stored_paths = []
 
-
-# Select every path with tracking_code as key
 for line in full_list:
-     if ('tracking_code' in line) and ('cells' in line):
+     if 'tracking_code' in line:
+         print(line)
          thistuple = []
-         #thistuple.append(line)
-         for x in range(1):
-             line.pop()
-         logging.debug(line)
          thistuple.append(line)
          newlist = copy.deepcopy(line)
-
-         # # For loop to remove the last element from the paths
          for element in range(0, len(line)-0):
              newlist.pop()
-             logging.debug(newlist)
+             print(newlist)
              thistuple.append(copy.copy(newlist))
          stored_paths.append(thistuple)
 
-no_ext = os.path.splitext(json_file)[0]
 
 
-
-""" Append data to keys """
 def getFromDict(dataDict, *mapList):
     global store_row, store_ind
     tempInfo = []
+    # notesList = []
 
     for arg in mapList:
         for mapper in arg:
@@ -78,6 +71,7 @@ def getFromDict(dataDict, *mapList):
                 if type(res) == dict:
                     for x in res.keys():
                         skipLevels = ("segment", "question", "notes", "validation", "cells", "rows", "cols")
+
                         if x not in skipLevels:
                             tempInfo.append((x, res[x]))
                             if x == 'col_index':
@@ -92,11 +86,23 @@ def getFromDict(dataDict, *mapList):
                             row_val = res[x]
                             row_dict = next((item for item in row_val if item["row_index"] == store_row))
                             tempInfo.append((x, row_dict))
+                        if x == "note_ID":
+                            note_val = res[x]
+                        if x == "notes":
+                            notesList = res[x]
+                            try:
+                                search_notes_track = next((item for item in notesList if item["note_ID"] == note_val))
+                                tempInfo.append((note_val, search_notes_track))
+                            except:
+                                print(" No notes here ")
 
 
-            logging.debug(tempInfo)
-            output_csv = sys.argv[4] # csv output argument
-            output_folder = sys.argv[3] # csv directory argument
+
+
+            print(tempInfo)
+            note_val = None
+            output_csv = sys.argv[3]  # csv output argument
+            output_folder = sys.argv[2]  # csv directory argument
             with open(output_folder + output_csv, 'a') as out:
                 csv_out = csv.writer(out)
             for row in tempInfo:
